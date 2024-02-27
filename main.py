76f05ebd12d5c81414845b8ec6d59b08bbd40cd9
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-27 17:34:17 -0300

    form validators

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 
@@ -23,10 +23,18 @@ def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'login feito com sucesso no email: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'login feito com sucesso no email: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+        
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
