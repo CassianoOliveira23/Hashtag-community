bbd9f1017d103ba6dd1306b89337c7074b0a53dc
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-28 14:17:42 -0300

    form validators

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'login feito com sucesso no email: {form_login.email.data}', 'alert-success')
+        flash(f'Login realizado com sucesso no email: {form_login.email.data}', 'alert-success')
         return redirect(url_for('home'))
     
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        flash(f'login feito com sucesso no email: {form_login.email.data}', 'alert-success')
+        flash(f'Conta criada com o email: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
         
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
