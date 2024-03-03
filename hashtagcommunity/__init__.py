65c3898003ae78eb42f69c5f538f9ec901219c84
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-03-02 23:10:07 -0300

    automatic redirection

--- hashtagcommunity/__init__.py
+++ hashtagcommunity/__init__.py
@@ -11,5 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
 
 from hashtagcommunity import routes
--- hashtagcommunity/routes.py
+++ hashtagcommunity/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from hashtagcommunity import app, database, bcrypt
 from hashtagcommunity.forms import FormCriarConta, FormLogin
 from hashtagcommunity.models import Usuario
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 
 lista_usuarios = ['Cassiano Oliveira', 'Ket Wolski Goetz', 'Alina Kovalenko', 'Angelina Mikhailichenko']
@@ -18,6 +18,7 @@ def contato():
 
 
 @app.route('/usuarios')
+@login_required
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
@@ -33,7 +34,12 @@ def login():
         if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
             flash(f'Login realizado com sucesso no email: {form_login.email.data}', 'alert-success')
-            return redirect(url_for('home'))
+            
+            next_parameter = request.args.get('next')
+            if next_parameter:
+                return redirect(next_parameter)
+            else:
+                return redirect(url_for('home'))
         else:
             flash(f'Falha no login. E-mail ou senha incorretos: {form_login.email.data}', 'alert-danger')
     
@@ -50,3 +56,25 @@ def login():
         return redirect(url_for('home'))
         
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash('Logout feito com sucesso', 'alert-success')
+    return redirect(url_for('home'))
+
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    return render_template('perfil.html')
+
+
+@app.route('/post/criar')
+@login_required
+def criar_post():
+    return render_template('criarpost.html')
+
+
